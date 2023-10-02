a4db331fb190dafe5a35a756e4a2e8bf94532773
Riad-Mehedi <129571327+Riad-Mehedi@users.noreply.github.com>
2023-10-02 23:20:39 +0600

    Add files via upload

---
+++ Week 1/I - Square Inequality.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long long double
+
+//#define for for(int i=0; i<n; i++)
+
+#define pb push_back
+#define pii pair<int,int>
+
+#define Test int t;cin >> t;while(t--)
+#define Q int q; cin>>q; while(q--)
+
+#define cY cout<<"YES"<<endl
+#define cN cout<<"NO"<<endl
+#define cy cout<<"Yes"<<endl
+#define cn cout<<"No"<<endl
+#define c1 cout<<-1<<endl
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+int main()
+{
+    int a,b,c; cin >> a >> b >> c;
+
+    if(a*a + b*b < c*c) cy;
+    else cn;
+
+    return 0;
+}
+
