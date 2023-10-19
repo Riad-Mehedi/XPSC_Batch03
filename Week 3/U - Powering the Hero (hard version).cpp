7fd5f7f8b04beab8dd143d2a25f6e3e624e77abc
Riad-Mehedi <129571327+Riad-Mehedi@users.noreply.github.com>
2023-10-19 23:27:13 +0600

    Add files via upload

---
+++ Week 3/U - Powering the Hero (hard version).cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define lli long long int
+#define ld long long double
+
+//#define for for(int i=0; i<n; i++)
+
+#define vi vector<int>
+#define vll vector<long long>
+#define pb push_back
+#define pii pair<int,int>
+
+#define Test int t;cin >> t;while(t--)
+#define Q int q; cin>>q; while(q--)
+
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+#define Yes cout<<"Yes"<<endl
+#define No cout<<"No"<<endl
+#define c1 cout<<-1<<endl
+#define endl "\n"
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+int main()
+{
+    Test
+    {
+        ll n; cin >> n;
+
+        vll v(n);
+        for(ll i=0; i<n; i++) cin >> v[i];
+
+        ll ans = 0;
+        priority_queue<ll> pq;
+        for(ll i=0; i<n; i++)
+        {
+            if(v[i] == 0)
+            {
+                if(pq.empty()) continue;
+                ans += pq.top();
+                pq.pop();
+            }
+            else
+                pq.push(v[i]);
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+
+
+
